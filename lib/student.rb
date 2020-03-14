268dc3ec4fb64c167c8b20f1d32d8bca69e2050b
sasha walker <sashawalker31@gmail.com>
2020-03-14 13:58:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash| #each student is a hash
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
